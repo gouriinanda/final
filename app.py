873348ba4f7750b87d55a3bc1c5407e6d4c7cdff
gouriinanda <gourinandakg@gmail.com>
2026-02-21 09:08:59 +0530

    Update app.run to include host and port configuration

--- app.py
+++ app.py
@@ -71,5 +71,10 @@ def cb():
 
     return render_template("indexc.html", reply=reply)
 
-if __name__ =="__main__":
-      app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=True
+    )
